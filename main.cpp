0626068a249b06e8cba8c28edda665ebce8547a3
InnoVah Tech <crimzor@kali.crimson>
2025-06-15 16:31:54 +0300

    Add main entry point

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "src/NovaCryptBot.h"
+
+int main() {
+    std::cout << "Hello, NovaCrypt - AI-Powered Precision Trading!" << std::endl;
+    NovaCryptBot bot;
+    bot.run();
+    return 0;
+} 
